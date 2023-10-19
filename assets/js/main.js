a5cf9f72876bdd47c25bf4b63c34b42b03d1d5c9
Shinoo99 <89867857+Shinoo99@users.noreply.github.com>
2023-10-19 23:05:14 +0300

    just needed some features

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+let spinner = document.querySelector(".spinner");
+
+window.addEventListener("scroll", function () {
+    let x = window.scrollY;
+    if (x > 500) {
+        spinner.style.opacity = 1
+    } else {
+        spinner.style.opacity = 0
+    }
+})
+
+let navBtn = document.querySelector(".navBar-bar i");
+let menu = document.querySelector(".responsive-nav");
+
+function showMenu() {
+    if (menu.style.height == 0) {
+        menu.style.height = menu.scrollHeight + "px";
+    } else {
+        menu.style.height = null;
+    }
+}
+navBtn.addEventListener("click", showMenu);
+
+let loginBtn = document.querySelector(".login");
+let loginDiv = document.querySelector(".login-div");
+
+function showLog() {
+    if (loginDiv.style.height == 0) {
+        loginDiv.style.height = loginDiv.scrollHeight + "px";
+    } else {
+        loginDiv.style.height = null;
+    }
+}
+
+loginBtn.addEventListener("click",showLog);
